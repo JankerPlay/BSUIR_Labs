ed7f7c09aa92b255c1fdb8ea4bdc28c68320fbf4
Kiryl Fralenka <57415393+JankerPlay@users.noreply.github.com>
2023-12-21 23:29:51 +0300

    Add files via upload

---
+++ 3 sem/ISP/Lab6/253504_Frolenko_Lab6/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _253504_Frolenko_Lab6
+{
+	public class Employee
+	{
+		public int EmployeeID { get; set; }
+		public bool IsActive { get; set; }
+		public string Name { get; set; }
+		public Employee(int employeeID, bool isActive, string name)
+		{
+			EmployeeID = employeeID;
+			IsActive = isActive;
+			Name = name;
+		}
+		public void DisplayInfo()
+		{
+			Console.WriteLine("Employee ID: " + EmployeeID);
+			Console.WriteLine("Name: " + Name);
+			Console.WriteLine("Active: " + IsActive);
+		}
+	}
+}
---
+++ 3 sem/ISP/Lab6/FileService/FileService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using _253504_Frolenko_Lab6;
+using Newtonsoft.Json;
+
+namespace FileService
+{
+	public class FileService<T> : IFileService<T> where T : class
+	{
+		public IEnumerable<T> ReadFile(string fileName)
+		{
+			string json = File.ReadAllText(fileName);
+			IEnumerable<T> data = JsonConvert.DeserializeObject<IEnumerable<T>>(json);
+			return data;
+		}
+		public void SaveData(IEnumerable<T> data, string fileName)
+		{
+			string json = JsonConvert.SerializeObject(data);
+			File.WriteAllText(fileName, json);
+		}
+	}
+}
