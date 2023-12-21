3d148e71dd0257de3fe6fcc5b94e415f923735c6
Kiryl Fralenka <57415393+JankerPlay@users.noreply.github.com>
2023-12-21 23:28:19 +0300

    Add files via upload

---
+++ 3 sem/ISP/Lab5/253504_Frolenko_Lab5/Program.cs
@@ -0,0 +1,217 @@
+﻿using Lab5_Domain;
+using SerializerLib;
+class Program
+{
+	static void Main(string[] args)
+	{
+		List<Restaurant> restaurants = new List<Restaurant>
+		{
+			new Restaurant
+			{
+				Name = "Restaurant1",
+				Kitchens = new List<Kitchen>
+				{
+					new Kitchen
+					{
+						Name = "Kitchen1",
+						Dishes = new List<Dish>
+						{
+							new Dish
+							{
+								Name = "Dish1",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient1", Quantity = 1 },
+									new Ingredient { Name = "Ingredient2", Quantity = 2 },
+								}
+							},
+							new Dish
+							{
+								Name = "Dish2",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient3", Quantity = 3 },
+									new Ingredient { Name = "Ingredient4", Quantity = 4 },
+								}
+							}
+						}
+					},
+					new Kitchen
+					{
+						Name = "Kitchen2",
+						Dishes = new List<Dish>
+						{
+							new Dish
+							{
+								Name = "Dish3",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient5", Quantity = 5 },
+									new Ingredient { Name = "Ingredient6", Quantity = 6 },
+								}
+							},
+							new Dish
+							{
+								Name = "Dish4",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient7", Quantity = 7 },
+									new Ingredient { Name = "Ingredient8", Quantity = 8 },
+								}
+							}
+						}
+					},
+					new Kitchen
+					{
+						Name = "Kitchen3",
+						Dishes = new List<Dish>
+						{
+							new Dish
+							{
+								Name = "Dish5",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient9", Quantity = 9 },
+									new Ingredient { Name = "Ingredient10", Quantity = 10 },
+								}
+							},
+							new Dish
+							{
+								Name = "Dish6",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient11", Quantity = 11 },
+									new Ingredient { Name = "Ingredient12", Quantity = 12 },
+								}
+							}
+						}
+					}
+				}
+			},
+			new Restaurant
+			{
+				Name = "Restaurant2",
+				Kitchens = new List<Kitchen>
+				{
+					new Kitchen
+					{
+						Name = "Kitchen4",
+						Dishes = new List<Dish>
+						{
+							new Dish
+							{
+								Name = "Dish7",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient13", Quantity = 13 },
+									new Ingredient { Name = "Ingredient14", Quantity = 14 },
+								}
+							},
+							new Dish
+							{
+								Name = "Dish8",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient15", Quantity = 15 },
+									new Ingredient { Name = "Ingredient16", Quantity = 16 },
+								}
+							}
+						}
+					},
+					new Kitchen
+					{
+						Name = "Kitchen5",
+						Dishes = new List<Dish>
+						{
+							new Dish
+							{
+								Name = "Dish9",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient17", Quantity = 17 },
+									new Ingredient { Name = "Ingredient18", Quantity = 18 },
+								}
+							},
+							new Dish
+							{
+								Name = "Dish10",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient19", Quantity = 19 },
+									new Ingredient { Name = "Ingredient20", Quantity = 20 },
+								}
+							}
+						}
+					},
+					new Kitchen
+					{
+						Name = "Kitchen6",
+						Dishes = new List<Dish>
+						{
+							new Dish
+							{
+								Name = "Dish11",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient21", Quantity = 21 },
+									new Ingredient { Name = "Ingredient22", Quantity = 22 },
+								}
+							},
+							new Dish
+							{
+								Name = "Dish12",
+								Ingredients = new List<Ingredient>
+								{
+									new Ingredient { Name = "Ingredient23", Quantity = 23 },
+									new Ingredient { Name = "Ingredient24", Quantity = 24 },
+								}
+							}
+						}
+					}
+				}
+			}
+		};
+		Serializer serializer = new Serializer();
+		serializer.SerializeXML(restaurants, "restaurants.xml");
+		serializer.SerializeJSON(restaurants, "restaurants.json");
+		serializer.SerializeByLINQ(restaurants, "restaurants_linq.xml");
+		List<Restaurant> xmlRestaurants = serializer.DeSerializeXML("restaurants.xml").ToList();
+		List<Restaurant> jsonRestaurants = serializer.DeSerializeJSON("restaurants.json").ToList();
+		List<Restaurant> linqXmlRestaurants = serializer.DeSerializeByLINQ("restaurants_linq.xml").ToList();
+		Console.WriteLine("File:");
+		foreach (var restaurant in xmlRestaurants)
+		{
+			Console.WriteLine("Restaurant Name: " + restaurant.Name);
+			foreach (var kitchen in restaurant.Kitchens)
+			{
+				Console.WriteLine("Kitchen Name: " + kitchen.Name);
+				foreach (var dish in kitchen.Dishes)
+				{
+					Console.WriteLine("Dish Name: " + dish.Name);
+					foreach (var ingredient in dish.Ingredients)
+					{
+						Console.WriteLine("Ingredient Name: " + ingredient.Name + ingredient.Quantity);
+					}
+				}
+			}
+		}
+		Console.WriteLine("File:");
+		foreach (var restaurant in xmlRestaurants)
+		{
+			Console.WriteLine("Restaurant Name: " + restaurant.Name);
+			foreach (var kitchen in restaurant.Kitchens)
+			{
+				Console.WriteLine("Kitchen Name: " + kitchen.Name);
+				foreach (var dish in kitchen.Dishes)
+				{
+					Console.WriteLine("Dish Name: " + dish.Name);
+					foreach (var ingredient in dish.Ingredients)
+					{
+						Console.WriteLine("Ingredient Name: " + ingredient.Name + " " + ingredient.Quantity);
+					}
+				}
+			}
+		}
+
+	}
+}
---
+++ 3 sem/ISP/Lab5/SerializerLib/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Lab5_Domain;
+using Newtonsoft.Json;
+
+namespace SerializerLib
+{
+	public class Serializer : ISerializer
+	{
+		public IEnumerable<Restaurant> DeSerializeByLINQ(string fileName)
+		{
+			XDocument doc = XDocument.Load(fileName);
+			var restaurants = doc.Root.Elements("Restaurant")
+				.Select(r => new Restaurant
+				{
+					Name = (string)r.Element("Name"),
+					Kitchens = r.Element("Kitchens").Elements("Kitchen")
+						.Select(k => new Kitchen
+						{
+							Name = (string)k.Element("Name"),
+							Dishes = k.Element("Dishes").Elements("Dish")
+								.Select(d => new Dish
+								{
+									Name = (string)d.Element("Name"),
+									Ingredients = d.Element("Ingredients").Elements("Ingredient")
+										.Select(i => new Ingredient
+										{
+											Name = (string)i.Element("Name"),
+											Quantity = (int)i.Element("Quantity")
+										})
+										.ToList()
+								})
+								.ToList()
+						})
+						.ToList()
+				})
+				.ToList();
+
+			return restaurants;
+		}
+
+		public IEnumerable<Restaurant> DeSerializeXML(string fileName)
+		{
+			XmlSerializer serializer = new XmlSerializer(typeof(List<Restaurant>));
+			using (StreamReader reader = new StreamReader(fileName))
+			{
+				List<Restaurant> restaurants = (List<Restaurant>)serializer.Deserialize(reader);
+				return restaurants;
+			}
+		}
+
+		public IEnumerable<Restaurant> DeSerializeJSON(string fileName)
+		{
+			string json = File.ReadAllText(fileName);
+			List<Restaurant> restaurants = JsonConvert.DeserializeObject<List<Restaurant>>(json);
+			return restaurants;
+		}
+
+		public void SerializeByLINQ(IEnumerable<Restaurant> restaurants, string fileName)
+		{
+			XDocument doc = new XDocument(
+				new XElement("Restaurants",
+					restaurants.Select(r =>
+						new XElement("Restaurant",
+							new XElement("Name", r.Name),
+							new XElement("Kitchens",
+								r.Kitchens.Select(k =>
+									new XElement("Kitchen",
+										new XElement("Name", k.Name),
+										new XElement("Dishes",
+											k.Dishes.Select(d =>
+												new XElement("Dish",
+													new XElement("Name", d.Name),
+													new XElement("Ingredients",
+														d.Ingredients.Select(i =>
+															new XElement("Ingredient",
+																new XElement("Name", i.Name),
+																new XElement("Quantity", i.Quantity)
+															)
+														)
+													)
+												)
+											)
+										)
+									)
+								)
+							)
+						)
+					)
+				)
+			);
+
+			doc.Save(fileName);
+		}
+
+		public void SerializeXML(IEnumerable<Restaurant> restaurants, string fileName)
+		{
+			XmlSerializer serializer = new XmlSerializer(typeof(List<Restaurant>));
+			using (StreamWriter writer = new StreamWriter(fileName))
+			{
+				serializer.Serialize(writer, restaurants);
+			}
+		}
+
+		public void SerializeJSON(IEnumerable<Restaurant> restaurants, string fileName)
+		{
+			string json = JsonConvert.SerializeObject(restaurants, Newtonsoft.Json.Formatting.Indented);
+			File.WriteAllText(fileName, json);
+		}
+	}
+}
